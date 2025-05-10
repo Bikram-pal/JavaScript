4b7521aa2ddd0961a7215323e81f4f4ada4a696c
Bikram-pal <palbikram557@gmail.com>
2025-05-11 00:07:55 +0530

    variable declare in this repo

---
+++ 01_basics/variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345
+let accountEmail = "palbikram557@gmail.com"
+var accountPassward = "12345" //does not use it only use let
+accountCity = "Kolkata"// do not use before ariable declare
+let accountState;
+
+
+// account_id= 123 // not allowed
+console.log(accountId);
+
+accountEmail= "bikram@gmail.com"//changable
+accountCity= "delhi"
+accountPassward= "98765"
+
+console.table([accountId, accountEmail, accountPassward, accountCity, accountState]);// print in a table 
+/*
+    not use var in code
+*/
+
+
+
