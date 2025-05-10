b51a916413faf488384b05c64396f7577369de2f
Bikram-pal <palbikram557@gmail.com>
2025-05-11 00:35:11 +0530

    data types in js

---
+++ dataType.js
@@ -0,0 +1,23 @@
+"use stricts"; // treat all JS code as a newer version 
+
+// alert(3+3); // we are useing node.js not browsers
+
+// console.log(3+3); console.log("Bikam") // dont use it because for readable issue 
+
+let name = "Bikram"// string
+let age = 18// int 
+let isLoggedIn = false// boolean
+
+// number ==> 2^53
+// bigint 
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value
+// undefined ==>
+// symbol ==> unique 
+
+// object
+
+console.log(typeof "Bikram");  //o/p string
+console.log(typeof null); // o/p object ****
+console.log(typeof undefined); // o/p undefined
