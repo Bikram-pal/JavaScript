4fb519605408d3d6477a3a0446b8f20d115ec67d
Bikram-pal <palbikram557@gmail.com>
2025-05-11 21:20:11 +0530

    operation in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,20 +1,20 @@
 let score= "33";
 
-console.log(typeof score);//string
-console.log(typeof (score));//string 
+// console.log(typeof score);//string
+// console.log(typeof (score));//string 
 
 let valueOfNumber= Number(score);
-console.log(typeof valueOfNumber);//number
+// console.log(typeof valueOfNumber);//number
 
 let point="abc33"
 convertNumberOfPoint=Number(point);
-console.table([typeof convertNumberOfPoint,convertNumberOfPoint]) // number,NaN
+// console.table([typeof convertNumberOfPoint,convertNumberOfPoint]) // number,NaN
 
 let temp= null;
 let temp1= undefined;
 let temp2= true;
 
-console.table([temp,temp1,temp2]); // null, undefined, true
+// console.table([temp,temp1,temp2]); // null, undefined, true
 // "33"==> 33
 // "33abc"==> NaN
 // true==> 1; false==> 0;
@@ -22,8 +22,42 @@ console.table([temp,temp1,temp2]); // null, undefined, true
 let isLoggedIn= 1;
 
 let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn) // true
+// console.log(booleanIsLoggedIn) // true
 // 1==> true;
 // 0==> false;
 // ""==> false;
 // "bikram"==> true;
+
+// **************************** Operation **************************
+
+let value=3
+let negValue= -value
+// console.log(negValue); // -3
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello";
+let str2= " Bikram";
+let str3= str1+str2;
+
+// console.log(str3);
+// console.log(1+"2");
+// console.log("2"+1);
+// console.log(1+1+"2");
+// console.log("2"+1+1);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, nm3;
+
+num1 = num2= numm3= 2+2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
