63f0a699311f448a8c40c549ad1667588ab53608
Bikram-pal <palbikram557@gmail.com>
2025-05-11 20:54:51 +0530

    conversion of data_types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score= "33";
+
+console.log(typeof score);//string
+console.log(typeof (score));//string 
+
+let valueOfNumber= Number(score);
+console.log(typeof valueOfNumber);//number
+
+let point="abc33"
+convertNumberOfPoint=Number(point);
+console.table([typeof convertNumberOfPoint,convertNumberOfPoint]) // number,NaN
+
+let temp= null;
+let temp1= undefined;
+let temp2= true;
+
+console.table([temp,temp1,temp2]); // null, undefined, true
+// "33"==> 33
+// "33abc"==> NaN
+// true==> 1; false==> 0;
+
+let isLoggedIn= 1;
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) // true
