65b72acfa3b122a2664605c5ed24378627010af3
Bikram-pal <palbikram557@gmail.com>
2025-05-12 19:58:39 +0530

    string in js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,38 @@
+const name= "bikram"
+const repocount = 50
+
+// console.log(name+ repocount); // does not use it
+
+// console.log(`hello everyone my name is ${name} and repo count is ${repocount}`);
+
+const gamename=new String("free-fire");
+// console.log(gamename);
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf("c"))
+
+const newString= gamename.substring(0,2)
+// console.log(newString);
+
+const anotherString = gamename.slice(0, -1)
+// console.log(anotherString);
+
+
+const nso = "   bikram    "
+// console.log(nso);
+// console.log(nso.trim())
+
+const url= "https://bikrampal.com/bikram%20pal"
+
+// console.log(url.replace("%20","-"))
+
+// console.log(url.includes("roy"))
+
+console.log(gamename.split("-"));
+
+
+
