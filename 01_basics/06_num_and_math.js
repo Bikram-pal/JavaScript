59df0dbcc63cd7ec2ba4f0f9677c24f415e3a43d
Bikram-pal <palbikram557@gmail.com>
2025-05-13 14:02:02 +0530

    make changes

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -43,4 +43,5 @@ console.log(Math.floor(Math.random()*(max - min+1)+min));
 console.log(Math.floor(Math.random()*(max - min+1)+min));
 console.log(Math.floor(Math.random()*(max - min+1)+min));
 console.log(Math.floor(Math.random()*(max - min+1)+min));
+ 
 
