32f5e443d11f1ad5e7c1d3c6b61bca8e31a085a4
Bikram-pal <palbikram557@gmail.com>
2025-05-13 13:58:51 +0530

    number and math in js

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,46 @@
+const score = 400
+// console.log(score);
+
+const balence = new Number(100)
+// console.log(balence);
+
+// console.log(balence.toString().length);
+// console.log(balence.toFixed(2));
+
+const newNum = 123.667
+
+// console.log(newNum.toPrecision(3));
+
+const number = 10000000;
+// console.log(number.toLocaleString('en-IN'));
+
+//********************************************** Math ***************************
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.987));
+// console.log(Math.ceil(4.287));
+// console.log(Math.floor(4.987));
+// console.log(Math.min(12,34,67));
+// console.log(Math.max(12,34,67));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10
+const max = 30
+
+
+
+console.log(Math.floor(Math.random()*(max - min+1)+min));
+console.log(Math.floor(Math.random()*(max - min+1)+min));
+console.log(Math.floor(Math.random()*(max - min+1)+min));
+console.log(Math.floor(Math.random()*(max - min+1)+min));
+console.log(Math.floor(Math.random()*(max - min+1)+min));
+console.log(Math.floor(Math.random()*(max - min+1)+min));
+console.log(Math.floor(Math.random()*(max - min+1)+min));
+console.log(Math.floor(Math.random()*(max - min+1)+min));
+console.log(Math.floor(Math.random()*(max - min+1)+min));
+console.log(Math.floor(Math.random()*(max - min+1)+min));
+
