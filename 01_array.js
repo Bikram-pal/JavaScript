e28625ee2ef753bc0cd2fb5f317e3dcbec2736a8
Bikram-pal <palbikram557@gmail.com>
2025-05-13 21:49:28 +0530

    array in js

---
+++ 01_array.js
@@ -0,0 +1,42 @@
+// Array
+const arr = [0,1,2,3]
+// console.log(arr);
+
+// console.log(arr[3]);
+
+const arr1 = ["ironman", " spiderman"];
+
+const arr2= new Array(1,2,3,4,5)
+// console.log(arr2[1]);
+
+// array methods
+
+arr.push(6)
+// console.log(arr);
+
+arr2.push(6)
+// console.log(arr2);
+
+arr2.pop() // delete last value of the arr
+
+// console.log(arr2);
+
+arr2.unshift(10); // shift in the front on the array 
+
+arr2.shift()// remove the front of the array
+
+// console.log(arr2.includes(9)); // ans it if i is exist in the arra.. it print false means it return boolean types 
+
+// console.log(arr2.indexOf(3)) // output of the index
+
+const arr3 = arr2.join()
+
+// console.log(arr2);
+// console.log(arr3); 
+
+// console.log(typeof arr2); // object 
+// console.log(typeof arr3); // string
+
+// slice, splice
+
+
