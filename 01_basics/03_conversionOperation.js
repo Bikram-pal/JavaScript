51748f8f3588f95998c174afdcbcf75e72cc4ec5
Bikram-pal <palbikram557@gmail.com>
2025-05-16 20:09:54 +0530

    some mistake are solved

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -54,9 +54,9 @@ let str3= str1+str2;
 // console.log(+true);
 // console.log(+"");
 
-let num1, num2, nm3;
+let num1, num2, num3;
 
-num1 = num2= numm3= 2+2
+num1 = num2= num3= 2+2
 
 let gamecounter = 100
 gamecounter++;
