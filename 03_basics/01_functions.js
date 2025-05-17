76e31dc4ddb561b6354d9e6e8df925e26620baf6
Bikram-pal <palbikram557@gmail.com>
2025-05-18 00:55:07 +0530

    array and object combination with function in js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,4 +36,34 @@ function logInUserMessage(username ="sam"){
     return `${username} justed logged in`
 }
 // console.log(logInUserMessage("bikram"));
-console.log(logInUserMessage());
+// console.log(logInUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+// ... is the rest operator 
+
+const user = {
+    username: "bikram",
+    price : 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+
+
+const myNewArray = [200,300,400,600]
+
+function returnSecondValue(array){
+    return array[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+
