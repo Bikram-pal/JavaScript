61154269e2a16b9245503f3d43bb8b4d8cab2d94
Bikram-pal <palbikram557@gmail.com>
2025-05-18 21:53:00 +0530

    scopes part 2 in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,4 +11,43 @@ if (true) {
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+
+function one (){
+    const username = "bikram"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website); // error
+    
+    two()
+}
+
+one()
+
+if(true){
+    const username = "bikram"
+    if(username === "bikram"){
+        const website= "youtube";
+        // console.log(username+website) 
+    }
+    // console.log(website); // error
+    
+}
+// console.log(username); error
+
+// ++++++++++++++++++++++++++++++++interesting +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(addone(5))
+
+function addone(num){
+    return num+1
+}
+
+// addtwo(5) // hosting 
+
+const addtwo = function(num){
+   return num + 2 
+}
